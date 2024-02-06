0836f8ec8bbcbad554ac422034d22a4425dbf713
Samir dahal <samirdahal069@gmail.com>
2024-02-07 04:28:10 +0545

    add Entity Framework Core In Memory and add model for Match entity

---
+++ SportradarCodingExercise/Models/Match.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Models
+{
+    public class Match
+    {
+        public Match()
+        {
+            // Generate a new GUID for MatchId when a Match is created
+            MatchId = Guid.NewGuid();
+            
+            //Set startTime when a Match is created
+            StartTime = DateTime.Now; 
+        }
+        [Required]
+        public Guid MatchId { get; set; }
+
+        [Required]
+        public string HomeTeam { get; set; }
+
+        [Required]
+        public string AwayTeam { get; set; }
+
+        [Range(0, int.MaxValue, ErrorMessage = "HomeTeamScore must be a non-negative integer.")]
+        public int HomeTeamScore { get; set; }
+
+        [Range(0, int.MaxValue, ErrorMessage = "AwayTeamScore must be a non-negative integer.")]
+
+        public int AwayTeamScore { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime? EndTime { get; set; }
+    }
+}
---
+++ SportradarCodingExercise/Services/DbContext/AppContext.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Models;
+
+namespace Services
+{
+    public class ApplictionDBContext : DbContext
+    {
+
+        public ApplictionDBContext(DbContextOptions<ApplictionDBContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Match> Matches { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseInMemoryDatabase("MatchDB");
+        }
+    }
+}
